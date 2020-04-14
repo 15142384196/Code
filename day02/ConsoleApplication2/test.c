3563691566bff432a19f49d1f57d788130ce0927
宇健 田 <15142384196@163.COM>
2020-04-14 17:31:47 +0800

    test

---
+++ day02/ConsoleApplication2/test.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void get_p(char* p)
+{
+	
+	printf("%d\n", (char*)p);
+
+}
+
+void get_p2( char**q)
+{
+	*q = 0xaabb;
+	printf("%p", *q);
+}
+int main(void)
+{
+	int a = 10;
+	int b = 20;
+	void *p = NULL;
+	(char)p = &a;
+	(char*)p = 20;
+	get_p((char*)p);
+	char* q = NULL;
+	q = &b;
+	get_p2(&q);
+
+
+
+
+
+
+
+	printf("\n");
+	system("pause");
+	return 0;
+
+}
