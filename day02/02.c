ae2caa92c30dc223bfcfbaee1b3a8cbaf1ab02c8
宇健 田 <15142384196@163.COM>
2020-04-15 17:56:32 +0800

    test

--- day02/02.c
+++ day02/02.c
@@ -3,11 +3,56 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+
+
+
+
+int main01(void)
+{
+	char src[] = "aqdqdqedqedqe";
+	int i;
+	char dst[100];
+	
+	for ( i = 0; src[i] != 0; i++)
+	{
+		dst[i] = src[i];
+	}
+	dst[i] = 0;
+	printf("%s", dst);
+	printf("\n");
+	system("pause");
+	return 0;
+
+}
+
+
+void my_strcpy(char* dst, char* src)
+{
+	int i = 0;
+	for (i = 0; src[i] != 0; i++)
+	{
+		dst[i] = src[i];
+	}
+	dst[i] = 0;
+}
+
+
 int main(void)
 {
+	char src[] = "aqdqdqedqedqe";
+	char dst[100];
+	my_strcpy(dst, src);
+	printf("%s", dst);
+
+
+
 
 	printf("\n");
 	system("pause");
 	return 0;
 
 }
+
+
+
